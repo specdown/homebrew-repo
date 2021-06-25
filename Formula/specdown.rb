27dbeae3f43fef2604ac6cae0c2cbfaca4c3b99a
Specdown <noreply@specdown.io>
2021-06-25 12:04:57 +0000

    fix: Update specdown to v0.44.0

--- Formula/specdown.rb
+++ Formula/specdown.rb
@@ -1,20 +1,14 @@
 class Specdown < Formula
   desc "Use your markdown documentation as tests"
   homepage "https://github.com/specdown/specdown"
-  url "https://github.com/specdown/specdown/archive/v0.43.7.tar.gz"
-  sha256 "596305d802ed557ffb602811a0c7a10357848e67f89bdcce82d725d7aaefff46"
-
-  bottle do
-    root_url "https://github.com/specdown/homebrew-repo/releases/download/specdown-0.43.7"
-    sha256 cellar: :any_skip_relocation, catalina:     "bfc922dd4c07958becb004442ea7d5d15c245884b32bcc38ef2d5808398ba02d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "7c26e11f0e5314a57789be3692ce7ea6adec91a0691bc6808fd9120f17ce3887"
-  end
+  url "https://github.com/specdown/specdown/archive/v0.44.0.tar.gz"
+  sha256 "17ea612eb9cf8b65c96d89d1a85e669bb7be06f3960589687440b0fe29aeb4f6"
 
   depends_on "rust" => :build
 
   resource("testdata") do
-    url "https://raw.githubusercontent.com/specdown/specdown/v0.43.7/README.md"
-    sha256 "5dc6c725ab63f61ce92b2ebb7c63136864d7ba55c98264a1e48258f48a908aa1"
+    url "https://raw.githubusercontent.com/specdown/specdown/v0.44.0/README.md"
+    sha256 "159572701dae798b44a40ee8a1721440d379759c7cf93284a2ae194686ec65e3"
   end
 
   def install
