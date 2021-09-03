8101351eed049cd103279041805554169ca5570e
Specdown <noreply@specdown.io>
2021-09-03 06:23:19 +0000

    fix: Update specdown to v0.47.1

--- Formula/specdown.rb
+++ Formula/specdown.rb
@@ -1,19 +1,13 @@
 class Specdown < Formula
   desc "Use your markdown documentation as tests"
   homepage "https://github.com/specdown/specdown"
-  url "https://github.com/specdown/specdown/archive/v0.47.0.tar.gz"
-  sha256 "9bf912bae64c3858fee77304b12abfb1049614c118cb8b477a3ffa3ebe41aef1"
-
-  bottle do
-    root_url "https://github.com/specdown/homebrew-repo/releases/download/specdown-0.47.0"
-    sha256 cellar: :any_skip_relocation, catalina:     "53f54bbcc293a72106b813c8b9a368f078841b10bba6786e6d69b14b280356d8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "1fe737961b9201dca423d8cda0d7cdaf5cafb5a28af1843dd2d286327f052124"
-  end
+  url "https://github.com/specdown/specdown/archive/v0.47.1.tar.gz"
+  sha256 "03a774f4d2f9a016f10c924f54cdf85caea41804da756d3713c02156393edfe3"
 
   depends_on "rust" => :build
 
   resource("testdata") do
-    url "https://raw.githubusercontent.com/specdown/specdown/v0.47.0/README.md"
+    url "https://raw.githubusercontent.com/specdown/specdown/v0.47.1/README.md"
     sha256 "256e375ecdf0c9f0567c05a20f62b222bd0efade004747484227d98fa307eed3"
   end
 
