c9f1a417470c1302deafa46f7b8069db2480f6b4
Specdown <noreply@specdown.io>
2021-10-21 13:15:52 +0000

    fix: Update specdown to v0.55.2

--- Formula/specdown.rb
+++ Formula/specdown.rb
@@ -1,24 +1,30 @@
 class Specdown < Formula
   desc "Use your markdown documentation as tests"
   homepage "https://github.com/specdown/specdown"
-  url "https://github.com/specdown/specdown/archive/v0.55.1.tar.gz"
-  sha256 "e084d92ce9c44c3762d176d2d9ad5eab64513ca4bfb6f971a3c405101cc9bddd"
-
-  bottle do
-    root_url "https://github.com/specdown/homebrew-repo/releases/download/specdown-0.55.1"
-    sha256 cellar: :any_skip_relocation, catalina:     "3333da3f763da3c130c972e2625e5550c2490ef184a5257687513d11ea2668be"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "a71139fca94c8d2bd2f9a17c3baadd50f75a67c8202d283594fa7f7e6cd09e54"
-  end
+  url "https://github.com/specdown/specdown/archive/v0.55.2.tar.gz"
+  sha256 "fb4befaa3786f598bd285325ef56e0844564febc24e3dae2767c6936bd603c2d"
 
   depends_on "rust" => :build
 
   resource("testdata") do
-    url "https://raw.githubusercontent.com/specdown/specdown/v0.55.1/README.md"
+    url "https://raw.githubusercontent.com/specdown/specdown/v0.55.2/README.md"
     sha256 "a9f658b79fbcb4b13f85cca439cd6e55a2d43a4ad47a05578c28e9c7f88bb8c0"
   end
 
   def install
     system "cargo", "install", "--locked", "--root", prefix, "--path", "."
+
+    # Install bash completion
+    output = Utils.safe_popen_read("#{bin}/specdown", "completion", "bash")
+    (bash_completion/"specdown").write output
+
+    # Install zsh completion
+    output = Utils.safe_popen_read("#{bin}/specdown", "completion", "zsh")
+    (zsh_completion/"_specdown").write output
+
+    # Install fish completion
+    output = Utils.safe_popen_read("#{bin}/specdown", "completion", "fish")
+    (fish_completion/"specdown.fish").write output
   end
 
   test do
