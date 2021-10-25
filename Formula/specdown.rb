daa2d182ed2854f90ac76dd5b0c593da55f386e1
Specdown <noreply@specdown.io>
2021-10-25 23:39:56 +0000

    fix: Update specdown to v0.55.7
    Closes #29.
    
    Signed-off-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- Formula/specdown.rb
+++ Formula/specdown.rb
@@ -1,19 +1,13 @@
 class Specdown < Formula
   desc "Use your markdown documentation as tests"
   homepage "https://github.com/specdown/specdown"
-  url "https://github.com/specdown/specdown/archive/v0.55.6.tar.gz"
-  sha256 "c03be1f9eae476752f1db46e27738e5415f53fbb5050f169491aa128ac68e742"
-
-  bottle do
-    root_url "https://github.com/specdown/homebrew-repo/releases/download/specdown-0.55.6"
-    sha256 cellar: :any_skip_relocation, catalina:     "9c710b586e6f96523dec5045de25e97e28e66760c8958340c21a7e0822f0df58"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "5329c991c4328d1cd1cd879ccb508f76b27754c2865b34e323a41ad0c471c5a8"
-  end
+  url "https://github.com/specdown/specdown/archive/v0.55.7.tar.gz"
+  sha256 "d96b02eceda07b9b7007eb6bebed6d8e391dcbf894c4f24ae5170f7cf4181175"
 
   depends_on "rust" => :build
 
   resource("testdata") do
-    url "https://raw.githubusercontent.com/specdown/specdown/v0.55.6/README.md"
+    url "https://raw.githubusercontent.com/specdown/specdown/v0.55.7/README.md"
     sha256 "a9f658b79fbcb4b13f85cca439cd6e55a2d43a4ad47a05578c28e9c7f88bb8c0"
   end
 
