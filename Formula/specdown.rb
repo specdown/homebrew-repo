3597d8b2561b38601b0fcf79009ada6033c66e6e
Specdown <noreply@specdown.io>
2022-07-26 06:32:48 +0000

    fix: Update specdown to v1.2.25

--- Formula/specdown.rb
+++ Formula/specdown.rb
@@ -1,20 +1,14 @@
 class Specdown < Formula
   desc "Use your markdown documentation as tests"
   homepage "https://github.com/specdown/specdown"
-  url "https://github.com/specdown/specdown/archive/v1.2.24.tar.gz"
-  sha256 "22d15722608c7927382472b13370b0fc308732915cf8391fe877d70d37d7e310"
-
-  bottle do
-    root_url "https://github.com/specdown/homebrew-repo/releases/download/specdown-1.2.24"
-    sha256 cellar: :any_skip_relocation, big_sur:      "e9ed7bd37772acf133475273d2e8412c16cf3a7af0169b7643ecc5d0d5312454"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "fb191962d2c521d4b0b9f34c87a22e481d4d295478dec349f36e6e758422a17a"
-  end
+  url "https://github.com/specdown/specdown/archive/v1.2.25.tar.gz"
+  sha256 "0fcbeb0581067951e7e4f0343f132534b83d57a2fffa2a3762bd9f51ad5dbd68"
 
   depends_on "help2man" => :build
   depends_on "rust" => :build
 
   resource("testdata") do
-    url "https://raw.githubusercontent.com/specdown/specdown/v1.2.24/README.md"
+    url "https://raw.githubusercontent.com/specdown/specdown/v1.2.25/README.md"
     sha256 "a9f658b79fbcb4b13f85cca439cd6e55a2d43a4ad47a05578c28e9c7f88bb8c0"
   end
 
