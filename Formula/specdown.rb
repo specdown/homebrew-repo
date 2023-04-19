37d4b103d2373e6eaff08817cc5d456c114bc3f8
Specdown <noreply@specdown.io>
2023-04-19 07:11:38 +0000

    fix: Update specdown to v1.2.44

--- Formula/specdown.rb
+++ Formula/specdown.rb
@@ -1,14 +1,14 @@
 class Specdown < Formula
   desc "Use your markdown documentation as tests"
   homepage "https://github.com/specdown/specdown"
-  url "https://github.com/specdown/specdown/archive/v1.2.41.tar.gz"
-  sha256 "1718f0ec18fb8d18c1cf71de6880d3d7eabe4d9eff35f269c376eb39018d0154"
+  url "https://github.com/specdown/specdown/archive/v1.2.44.tar.gz"
+  sha256 "28ef4c4af08df224e65dd0f8bf4a583503f357454bc7317682bae92abe8ee3a2"
 
   depends_on "help2man" => :build
   depends_on "rust" => :build
 
   resource("testdata") do
-    url "https://raw.githubusercontent.com/specdown/specdown/v1.2.41/README.md"
+    url "https://raw.githubusercontent.com/specdown/specdown/v1.2.44/README.md"
     sha256 "a9f658b79fbcb4b13f85cca439cd6e55a2d43a4ad47a05578c28e9c7f88bb8c0"
   end
 
