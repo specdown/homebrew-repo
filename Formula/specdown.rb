416e89c11f01e6ca6b369fea8c167bbfd4224114
Specdown <noreply@specdown.io>
2023-07-20 06:53:56 +0000

    fix: Update specdown to v1.2.64

--- Formula/specdown.rb
+++ Formula/specdown.rb
@@ -1,21 +1,21 @@
 class Specdown < Formula
   desc "Use your markdown documentation as tests"
   homepage "https://github.com/specdown/specdown"
-  url "https://github.com/specdown/specdown/archive/v1.2.41.tar.gz"
-  sha256 "1718f0ec18fb8d18c1cf71de6880d3d7eabe4d9eff35f269c376eb39018d0154"
+  url "https://github.com/specdown/specdown/archive/v1.2.64.tar.gz"
+  sha256 "127bc59f650985ca6a8f743634e5322a21500069f1697db4efb37e1ec1ff0ba7"
 
   depends_on "help2man" => :build
   depends_on "rust" => :build
 
   resource("testdata") do
-    url "https://raw.githubusercontent.com/specdown/specdown/v1.2.41/README.md"
+    url "https://raw.githubusercontent.com/specdown/specdown/v1.2.64/README.md"
     sha256 "a9f658b79fbcb4b13f85cca439cd6e55a2d43a4ad47a05578c28e9c7f88bb8c0"
   end
 
   def install
     system "cargo", "install", "--locked", "--root", prefix, "--path", "."
 
-    generate_completions_from_executable(bin/"specdown", "--completion", shells: [
+    generate_completions_from_executable(bin/"specdown", "completion", shells: [
       :bash,
       :fish,
       :zsh,
