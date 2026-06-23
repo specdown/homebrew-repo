d81be30e93bed44014af05861baff6a34cde01a1
Specdown <noreply@specdown.io>
2026-06-23 06:38:05 +0000

    fix: Update specdown to v1.3.7

--- Formula/specdown.rb
+++ Formula/specdown.rb
@@ -6,28 +6,28 @@ class Specdown < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/specdown/specdown/releases/download/v1.3.3/specdown-aarch64-apple-darwin"
-      sha256 "bd6bfc3370a9c859c097921aa15d855988920b922d41373aee120542ed168a9a"
+      url "https://github.com/specdown/specdown/releases/download/v1.3.7/specdown-aarch64-apple-darwin"
+      sha256 "15c009aecd82da4ed09527e335fd8cea16999b53aeb95b771c0bc9007b58d082"
     end
     on_intel do
-      url "https://github.com/specdown/specdown/releases/download/v1.3.3/specdown-x86_64-apple-darwin"
-      sha256 "707a8fdc56159aaf1892592d2e7fee90e01560f8e318e0d3eae03188a7785158"
+      url "https://github.com/specdown/specdown/releases/download/v1.3.7/specdown-x86_64-apple-darwin"
+      sha256 "2422a4e056980753781e2e419f12623e81c97b31519742fceb73eeee3d1145bc"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/specdown/specdown/releases/download/v1.3.3/specdown-x86_64-unknown-linux-gnu"
-      sha256 "8c91a5e19d60379d6322002708b348942037a375e7232759e066fcebc58f2018"
+      url "https://github.com/specdown/specdown/releases/download/v1.3.7/specdown-x86_64-unknown-linux-gnu"
+      sha256 "40f2e1ee2b2ca291c82b24f7d875d0ba4955b5098c857e34e8686f63ac9a83b4"
     end
     on_arm do
-      url "https://github.com/specdown/specdown/releases/download/v1.3.3/specdown-aarch64-unknown-linux-gnu"
-      sha256 "c30c535b477743cac8400b8ff556d34f7b939deb45e70bc08cf6bd5e5f774b33"
+      url "https://github.com/specdown/specdown/releases/download/v1.3.7/specdown-aarch64-unknown-linux-gnu"
+      sha256 "6d9c68e4a1ac15364dbb46a4bc294ff58113e919900415a7f4fc07b142ed85df"
     end
   end
 
   resource("testdata") do
-    url "https://raw.githubusercontent.com/specdown/specdown/v1.3.3/README.md"
+    url "https://raw.githubusercontent.com/specdown/specdown/v1.3.7/README.md"
     sha256 "b4a0f54551331989fc3f4776188dfcbb901da2e2ff511cd801ba6d30fb662e8c"
   end
 
